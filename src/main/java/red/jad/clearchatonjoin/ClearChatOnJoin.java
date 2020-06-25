04f2d71ecda0ebcb6294b74451ce26bb41e7cc10
jadc <chehimj@gmail.com>
2020-06-24 18:10:05 -0600

    remove unneccesities

--- src/main/java/red/jad/clearchatonjoin/ClearChatOnJoin.java
+++ src/main/java/red/jad/clearchatonjoin/ClearChatOnJoin.java
@@ -5,30 +5,16 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = "clearchatonjoin", useMetadata = true, clientSideOnly = true, serverSideOnly = false)
-
+@Mod.EventBusSubscriber
 public class ClearChatOnJoin{
 	@EventHandler
-    public void postInit(FMLPostInitializationEvent e){
- 		if(e.getSide() == Side.CLIENT) MinecraftForge.EVENT_BUS.register(this);
- 	}
-	
-	@SideOnly(Side.CLIENT)
-	@SubscribeEvent
-	public void onLogin(ClientConnectedToServerEvent e) {
-    	MinecraftForge.EVENT_BUS.register(new ChatClearer());
-	}
-    
+    public void postInit(FMLPostInitializationEvent e){ MinecraftForge.EVENT_BUS.register(this); }
+	public void onLogin(ClientConnectedToServerEvent e) { MinecraftForge.EVENT_BUS.register(new ChatClearer()); }
 	class ChatClearer {
-		@SideOnly(Side.CLIENT)
-		@SubscribeEvent(priority = EventPriority.LOWEST)
 		public void onTick(PlayerTickEvent e) {
 			Minecraft.getMinecraft().ingameGUI.getChatGUI().clearChatMessages(false);
 			if(e.player.ticksExisted >= 20) MinecraftForge.EVENT_BUS.unregister(this);
